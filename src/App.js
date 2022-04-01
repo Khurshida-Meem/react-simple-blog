4831b8b5c5def404421187a2448bd7f061f959b0
Khurshida-Meem <meemrumi59@gmail.com>
2022-04-01 09:22:36 +0600

    applied route parameters

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Post from './Components/Post';
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <Router>
-      <Navbar />
+        <Navbar />
         <Switch>
           <Route exact path='/'>
-            <Home></Home>
+            <Home />
           </Route>
           <Route path='/about'>
-            <About></About>
+            <About />
           </Route>
           <Route path='/contact'>
-            <Contact></Contact>
+            <Contact />
           </Route>
+          {/* <Route path='/post/:post_id'>
+            <Post />
+          </Route> */}
+          <Route exact path="/post/:post_id" render={props => <Post {...props.match.params} />} />
         </Switch>
       </Router>
     )
