22a9b057477208f50f3154721f2d451ea1a72395
Khurshida-Meem <meemrumi59@gmail.com>
2022-04-01 16:17:19 +0600

    added redux

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,14 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
-import img from '../img/pokeball.png'
+import img from '../img/pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-    state = {
-        posts: [ ]
-    }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            this.setState({
-                posts: res.data.slice(0,10)
-            })
-        })
-
-    }
+    
     render() {
-        const {posts} = this.state;
+        const {posts} = this.props;
         const postList = posts.length ? (
+            
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
@@ -44,4 +34,11 @@ class Home extends Component {
     }
 };
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    }
+}
+
+
+export default connect(mapStateToProps)(Home);
---
+++ src/Reducer/rootReducer.js
@@ -0,0 +1,25 @@
+const initState = {
+    posts: [ 
+        {
+            id: 1,
+            title: "doloribus ad provident suscipit at",
+            body: "qui consequuntur ducimus possimus quisquam amet similique suscipit porro ipsam amet eos veritatis officiis exercitationem vel fugit aut necessitatibus totam omnis rerum consequatur expedita quidem cumque explicabo"
+            },
+            {
+            id: 2,
+            title: "asperiores ea ipsam voluptatibus modi minima quia sint",
+            body: "repellat aliquid praesentium dolorem quo sed totam minus non itaque nihil labore molestiae sunt dolor eveniet hic recusandae veniam tempora et tenetur expedita sunt"
+            },
+            {
+            id: 3,
+            title: "dolor sint quo a velit explicabo quia nam",
+            body: "eos qui et ipsum ipsam suscipit aut sed omnis non odio expedita earum mollitia molestiae aut atque rem suscipit nam impedit esse"
+            },
+    ]
+}
+
+const rootReducer = (state= initState, action) => {
+    return state;
+}
+
+export default rootReducer;
